1f13debf83ddf496ddc82c4a423024866c18e60f
denisrands <denisrands@gmail.com>
2023-06-16 15:56:58 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,10 @@
 /**
  * main - Entry point of the program
  *
- * Return: Always 0 (successful execution)
+ * Return: (1)(successful execution)
  */
 int main(void)
 {
 write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-return (0);
+return (1);
 }
