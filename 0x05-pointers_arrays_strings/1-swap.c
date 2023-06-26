51a189a79f7599394342182c7d2a98feb955b2dc
denisrands <denisrands@gmail.com>
2023-06-26 16:44:25 -0700

    1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,8 +1,7 @@
 #include "main.h"
 /**
-* swap_int – take in two variables interpret and swap them
-* @author Anisah
-* @a: swaps and store address of b
+* swap_int – take in two variable integers and swaps them
+* @a: swaps and stores address of b
 * @b: swaps and stores address of a
 * Return:  0
 */
