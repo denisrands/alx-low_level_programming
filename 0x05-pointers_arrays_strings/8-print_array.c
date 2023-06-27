6346b0110815a732f140b451131c134b5bb19de7
denisrands <denisrands@gmail.com>
2023-06-26 18:52:00 -0700

    8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -8,7 +8,7 @@
 void print_array(int *a, int n)
 {
 int index_of_array;
-for (index_of_array = 0; index_of_array <n; index_of_array++)
+for (index_of_array = 0; index_of_array < n; index_of_array++)
 {
 printf("%d", a[index_of_array]);
 {
