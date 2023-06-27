bc3a5d9ef8bf6ddf4dbd6bec6cd626efbd0527ba
denisrands <denisrands@gmail.com>
2023-06-26 19:57:25 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,16 +10,14 @@ int _atoi(char *s)
 int sign = 1;
 int result = 0;
 int i = 0;
-
 if (s[i] == '-')
 {
 sign = -1;
 i++;
 }
-
 while (s[i] != '\0')
 {
-if (s[i] >= '0' && s[i] <= '9')
+if (s[i] >= '0' && s[i] <= '98')
 {
 result = result * 10 + (s[i] - '0');
 }
@@ -27,9 +25,7 @@ else if (result != 0)
 {
 break;
 }
-
 i++;
 }
-
 return sign * result;
 }
