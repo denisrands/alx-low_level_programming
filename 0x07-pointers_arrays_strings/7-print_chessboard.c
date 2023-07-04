f3a51a33bcfc4a5c0bc6c1d63d54fe2162ddc9d5
denisrands <denisrands@gmail.com>
2023-07-04 13:45:02 -0700

    7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,16 +1,18 @@
 #include "main.h"
-#include <stdio.h>
-
+/**
+* print_chessboard - Entry point
+* @a: array
+* Return: Always 0 (Success)
+*/
 void print_chessboard(char (*a)[8])
 {
-int row, col;
+int i;
+int j;
 
-for (row = 0; row < 8; row++)
+for (i = 0; i < 8; i++)
 {
-for (col = 0; col < 8; col++)
-{
-_putchar(a[row][col]);
-}
+for (j = 0; j < 8; j++)
+_putchar(a[i][j]);
 _putchar('\n');
 }
 }
